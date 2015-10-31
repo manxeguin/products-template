eaf37b628d1c8e4e735dae31cc90c1587ab2202c
manxeguin1987 <manxeguin1987@gmail.com>
2015-10-31 19:57:24 +0100

    firstCommit

---
+++ scripts/init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('ul.tabs').tabs();
+    $('.materialboxed').materialbox();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
